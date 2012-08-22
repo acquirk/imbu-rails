6628fdaa5552ad8e03bf23e252165332e66ad3f2
Adam Quirk <bullemhead@gmail.com>
2012-08-22 00:34:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imbu::Application.config.secret_token = '119c5f54c04bddeb3a50facd933a9dca26fc3a8042eb7e79da9f9bdb6047f9f5b8134fc25c15b70ca343fb039f383ed8332c9cc07f5827ac92d54ac14b536fc6'
